524d45f167ca3f3551fbdf459a8c9b4f915208fd
WaiWai9999 <zinwaiwainwe9999@gmail.com>
2023-06-02 19:08:57 +0900

    design変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,132 +1,158 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import { Row, Col, Card, Table } from "react-bootstrap";
-import Layout from "../components/layout";
 import SEO from "../components/seo";
-import homeImg from "../images/Design1.png";
-import aiImg1 from "../images/AI.jpg";
-import aiImg2 from "../images/ai2.jpg";
-import webImg1 from "../images/WEB.jpg";
-import webImg2 from "../images/web2.jpg";
-import iotImg1 from "../images/IOT.jpg";
-import iotImg2 from "../images/iot2.jpg";
+import Layout from "../components/layout";
+import homeImg from "../images/home.png";
 import "../style/common.scss";
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <Table className="homePage">
-      <tbody>
-        <tr>
-          <td colSpan="2">
-            <img src={homeImg} style={{ width: "100%" }} alt="Home" />
-          </td>
-        </tr>
+const IndexPage = ({ data }) => {
+  const articlesPerRow = 2;
 
-        {data.allMicrocmsInformation.edges.map(({ node }) => (
-          <React.Fragment key={node.id}>
-            <tr>
-              <td colSpan="2" className="title">
-                {node.title}
-              </td>
-            </tr>
-            <tr>
-              <td className="article">
-                <Card className="card">
-                  {node.title === "IOT" && (
-                    <Card.Img
-                      variant="top"
-                      src={iotImg1}
-                      alt="IOT記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  {node.title === "AI" && (
-                    <Card.Img
-                      variant="top"
-                      src={aiImg1}
-                      alt="AI記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  {node.title === "WEB" && (
-                    <Card.Img
-                      variant="top"
-                      src={webImg1}
-                      alt="WEB記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  <Card.Body>
-                    <Card.Title style={{ fontSize: "1rem" }}>
-                      <Link
-                        to={`/information/${node.id}`}
-                        className="btn btn-primary link-design"
-                      >
-                        記事を読む
-                      </Link>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              </td>
-              <td className="article">
-                <Card style={{ width: "100%" }}>
-                  {node.title === "IOT" && (
-                    <Card.Img
-                      variant="top"
-                      src={iotImg2}
-                      alt="IOT記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  {node.title === "AI" && (
-                    <Card.Img
-                      variant="top"
-                      src={aiImg2}
-                      alt="AI記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  {node.title === "WEB" && (
-                    <Card.Img
-                      variant="top"
-                      src={webImg2}
-                      alt="WEB記事"
-                      style={{ height: "300px", objectFit: "cover" }}
-                    />
-                  )}
-                  <Card.Body>
-                    <Card.Title style={{ fontSize: "1rem" }}>
-                      <Link
-                        to={`/information/${node.id}`}
-                        className="btn btn-primary link-design"
-                      >
-                        記事を読む
-                      </Link>
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              </td>
-            </tr>
-            <tr></tr>
-          </React.Fragment>
-        ))}
-      </tbody>
-    </Table>
-  </Layout>
-);
+  const createArticleCards = articles => {
+    const articleCards = [];
+
+    for (let i = 0; i < articles.length; i += articlesPerRow) {
+      const row = articles.slice(i, i + articlesPerRow).map(({ node }) => (
+        <Col key={node.id} md={6} style={{ marginBottom: "1rem" }}>
+          <Card>
+            <Link to={`/information/${node.id}`}>
+              <Card.Img
+                variant="top"
+                src={node.image.url}
+                style={{ height: "300px", objectFit: "cover" }}
+              />
+              <Card.Body>
+                <Card.Title style={{ height: "40px", objectFit: "cover" }}>
+                  {node.title}
+                </Card.Title>
+                <Link
+                  to={`/information/${node.id}`}
+                  className="btn btn-primary link-design"
+                >
+                  記事を読む
+                </Link>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
+      ));
+
+      articleCards.push(
+        <Row key={i} style={{ justifyContent: "space-between" }}>
+          {row}
+        </Row>
+      );
+    }
+
+    return articleCards;
+  };
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Table>
+        <tbody
+          style={{
+            borderCollapse: "collapse",
+            borderColor: "cornflowerblue 1px",
+          }}
+        >
+          <tr>
+            <td colSpan="2">
+              <img
+                src={homeImg}
+                style={{ width: "102%", margin: "45px 0px 5px -12px" }}
+                alt="Home"
+              />
+            </td>
+          </tr>
+          <tr className="title borderStyle">
+            <td>IOT</td>
+          </tr>
+          <tr className="borderStyle">
+            <td colSpan="2">{createArticleCards(data.iot.edges)}</td>
+          </tr>
+          <tr className="title borderStyle">
+            <td>WEB</td>
+          </tr>
+          <tr className="borderStyle">
+            <td colSpan="2">{createArticleCards(data.web.edges)}</td>
+          </tr>
+          <tr className="title borderStyle">
+            <td>AI</td>
+          </tr>
+          <tr className="borderStyle">
+            <td colSpan="2">{createArticleCards(data.ai.edges)}</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Layout>
+  );
+};
 
 export default IndexPage;
 export const query = graphql`
   query {
-    allMicrocmsInformation(limit: 4, sort: { fields: date, order: DESC }) {
+    iot: allMicrocmsInformation(
+      limit: 4
+      sort: { fields: date, order: DESC }
+      filter: { category: { category: { eq: "IOT" } } }
+    ) {
       edges {
         node {
           id
-          title
           date
+          title
           category {
             category
+          }
+          image {
+            url
+            height
+            width
+          }
+        }
+      }
+    }
+    ai: allMicrocmsInformation(
+      limit: 4
+      sort: { fields: date, order: DESC }
+      filter: { category: { category: { eq: "AI" } } }
+    ) {
+      edges {
+        node {
+          id
+          date
+          title
+          category {
+            category
+          }
+          image {
+            url
+            height
+            width
+          }
+        }
+      }
+    }
+    web: allMicrocmsInformation(
+      limit: 4
+      sort: { fields: date, order: DESC }
+      filter: { category: { category: { eq: "WEB" } } }
+    ) {
+      edges {
+        node {
+          id
+          date
+          title
+          category {
+            category
+          }
+          image {
+            url
+            height
+            width
           }
         }
       }
