313cd713b4cf846e1a6d094154c3b63b830191a3
WaiWai9999 <zinwaiwainwe9999@gmail.com>
2023-06-02 19:07:19 +0900

    Zoho form交換

--- src/components/contact-home.js
+++ src/components/contact-home.js
@@ -20,7 +20,7 @@ const ContactPage = () => {
       </Row>
       <iframe
         class="responsive-iframe"
-        src="https://forms.zohopublic.jp/zinwaiwainwe9999/form/ProductFeedback/formperma/tSM_DHuruFGY6V7MfGDnyPYG5nZwIbcI_-L9KgaKz4I"
+        src="https://forms.zohopublic.jp/zinwaiwainwe9999/form/AssetRequestForm/formperma/ZvbGk7T_omPBy6o3BSwfdhBp-RsOGfqE4Zb2A0DePxQ"
       ></iframe>
     </div>
   );
