24da28f64660688839fb2dc27b7b5404020803c5
WaiWai9999 <zinwaiwainwe9999@gmail.com>
2023-06-08 21:21:37 +0900

    category追加

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// /**
+//  * Implement Gatsby's Node APIs in this file.
+//  *
+//  * See: https://www.gatsbyjs.org/docs/node-apis/
+//  */
 
 // You can delete this file if you're not using it
 // gatsby-node.js
 // 「5.3.3 インフォメーション詳細ページ（動的ページ）の作成」
-const path = require("path");
 
-exports.createPages = ({ graphql, actions }) => {
+const path = require("path");
+exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
-  return graphql(
-    `
-      {
-        allMicrocmsInformation {
-          edges {
-            node {
-              id
+  const result = await graphql(`
+    query {
+      allMicrocmsInformation {
+        edges {
+          node {
+            id
+            category {
+              category
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors;
     }
+  `);
 
-    result.data.allMicrocmsInformation.edges.forEach(edge => {
-      createPage({
-        path: `/information/${edge.node.id}`,
-        component: path.resolve(`./src/templates/info-posts.js`),
-        context: {
-          id: edge.node.id,
-        },
-      });
+  if (result.errors) {
+    reporter.panicOnBuild("Error while running GraphQL query.");
+    return;
+  }
+
+  const informationTemplate = require.resolve("./src/templates/info-posts.js");
+  result.data.allMicrocmsInformation.edges.forEach(({ node }) => {
+    createPage({
+      path: `/information/${node.category.category}/${node.id}`,
+      component: informationTemplate,
+      context: {
+        id: node.id,
+      },
     });
   });
 };
