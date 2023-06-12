7db0619928b0c35a69bd559b88dd01070cdb6f99
WaiWai9999 <zinwaiwainwe9999@gmail.com>
2023-06-12 14:49:56 +0900

    add email react-share

--- src/pages/information.js
+++ src/pages/information.js
@@ -9,6 +9,8 @@ import {
   TwitterIcon,
   LineShareButton,
   LineIcon,
+  EmailShareButton,
+  EmailIcon,
 } from "react-share";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -17,7 +19,7 @@ import "../style/common.scss";
 const InformationPage = ({ location, data }) => {
   const [shareUrl, setShareUrl] = useState("");
   const [selectedCategory, setSelectedCategory] = useState(
-    location.state?.category ?? null
+    location.state && location.state.category ? location.state.category : null
   );
 
   const categories = uniqBy(
@@ -34,8 +36,10 @@ const InformationPage = ({ location, data }) => {
   };
 
   useEffect(() => {
-    setShareUrl(window.location.href);
+    setShareUrl(typeof window !== "undefined" ? window.location.href : "");
   }, []);
+
+  const emailBody = `記事一覧を共有します。\n`;
 
   return (
     <Layout>
@@ -83,21 +87,37 @@ const InformationPage = ({ location, data }) => {
           )
           .map(({ node }) => (
             <Col xs={12} md={6} key={node.id}>
-              <Card style={{ marginTop: "1rem" }}>
-                <Card.Body>
-                  <Card.Title>
-                    <Link
-                      to={`/information/${node.category.category}/${node.id}`}
-                    >
-                      {node.title}
-                    </Link>
-                  </Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {node.date}
-                    {``}
-                    {node.category.category}
-                  </Card.Subtitle>
-                  <Card.Text>{node.exerpt}</Card.Text>
+              <Card style={{ marginTop: "1rem", display: "flex" }}>
+                <Card.Body style={{ display: "flex", alignItems: "center" }}>
+                  <div style={{ flex: 1 }}>
+                    <div>
+                      <Card.Title>
+                        <Link
+                          to={`/information/${node.category.category}/${node.id}`}
+                        >
+                          {node.title}
+                        </Link>
+                      </Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {node.date}
+                      </Card.Subtitle>
+                    </div>
+                    {/* <div>
+                      <Card.Text>{node.exerpt}</Card.Text>
+                    </div> */}
+                  </div>
+                  <div>
+                    <Card.Img
+                      variant="top"
+                      src={node.image.url}
+                      style={{
+                        marginLeft: "5px",
+                        width: "150px",
+                        height: "100px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
                 </Card.Body>
               </Card>
             </Col>
@@ -110,17 +130,26 @@ const InformationPage = ({ location, data }) => {
         <p>Share this page:</p>
         <FacebookShareButton
           url={shareUrl}
+          className="Demo__some-network__share-button"
           quote={"Dummy text!"}
           hashtag="#muo"
         >
           <FacebookIcon size={32} round />
         </FacebookShareButton>
+
         <TwitterShareButton url={shareUrl} quote={"Dummy text!"} hashtag="#muo">
           <TwitterIcon size={32} round />
         </TwitterShareButton>
         <LineShareButton url={shareUrl} quote={"Dummy text!"}>
           <LineIcon size={32} round />
         </LineShareButton>
+        <EmailShareButton
+          url={shareUrl}
+          subject="Devpediacodeの記事一覧共有"
+          body={emailBody}
+        >
+          <EmailIcon size={32} round />
+        </EmailShareButton>
       </div>
       <Link to="/" className="btn btn-primary link-design">
         Back to Home
@@ -143,6 +172,11 @@ export const query = graphql`
           date(formatString: "YYYY 年 MM 月 DD 日")
           category {
             category
+          }
+          image {
+            url
+            height
+            width
           }
         }
       }
