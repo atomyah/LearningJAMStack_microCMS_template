26bf42403c2bb14b26f889f7c688ae347ac3db68
Cielo <32344752+cielo796@users.noreply.github.com>
2023-09-19 05:06:52 +0900

    Gtag設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,38 +6,46 @@ module.exports = {
     siteUrl: `https://devpediacode.com`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-7RYQZ5JFJR",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "OPT-TW58LC5",
-        // Enables Google Optimize Experiment ID
-        experimentId: "KWRJbTsOSdi9368QGbmOug",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "0",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "devpediacode.com",
-        // defaults to false
-        enableWebVitalsTracking: true,
-      },
-   },
- 
+  //   {
+  //     resolve: `gatsby-plugin-google-analytics`,
+  //     options: {
+  //       // The property ID; the tracking code won't be generated without it
+  //       trackingId: "G-7RYQZ5JFJR",
+  //       // Defines where to place the tracking script - `true` in the head and `false` in the body
+  //       head: true,
+  //       // Setting this parameter is optional
+  //       anonymize: true,
+  //       // Setting this parameter is also optional
+  //       respectDNT: true,
+  //       // Avoids sending pageview hits from custom paths
+  //       exclude: ["/preview/**", "/do-not-track/me/too/"],
+  //       // Delays sending pageview hits on route update (in milliseconds)
+  //       pageTransitionDelay: 0,
+  //       // Enables Google Optimize using your container Id
+  //       optimizeId: "OPT-TW58LC5",
+  //       // Enables Google Optimize Experiment ID
+  //       experimentId: "KWRJbTsOSdi9368QGbmOug",
+  //       // Set Variation ID. 0 for original 1,2,3....
+  //       variationId: "0",
+  //       // Defers execution of google analytics script after page load
+  //       defer: false,
+  //       // Any additional optional fields
+  //       sampleRate: 5,
+  //       siteSpeedSampleRate: 10,
+  //       cookieDomain: "devpediacode.com",
+  //       // defaults to false
+  //       enableWebVitalsTracking: true,
+  //     },
+  //  },
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingIds: ["G-7RYQZ5JFJR"],  // 控えておいた、測定IDを記載します。
+      pluginConfig: {
+        head: true  // headタグに記載されるようにコンフィグを設定します。
+      }
+    }
+  }
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-microcms`,
